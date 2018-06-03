d8b1f1c31f4a9a174402bb9c8517d9e36bd42a01
Ivan Ermilov <earthquakesan@gmail.com>
2018-06-03 16:30:52 +0200

    use docker to run packer

--- aws/packer/make-ami.sh
+++ aws/packer/make-ami.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+# Use the unofficial bash strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail; export FS=$'\n\t'
 
-wget https://releases.hashicorp.com/packer/1.2.3/packer_1.2.3_linux_amd64.zip
-unzip -o packer_1.2.3_linux_amd64.zip
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GITHUB_ROOT="$DIR/../../"
+echo $GITHUB_ROOT
 
-./packer validate cmem-docker.json
+docker run -it --rm\
+    -v $DIR/cmem-docker.json:/cmem-docker.json:ro\
+    -v $GITHUB_ROOT:/cmem-docker:ro\
+    hashicorp/packer:light validate /cmem-docker.json
 
-./packer build \
+docker run -it --rm\
+    -v $DIR/cmem-docker.json:/cmem-docker.json\
+    -v $GITHUB_ROOT:/cmem-docker:ro\
+    hashicorp/packer:light build\
     -var "aws_access_key=$AWS_ACCESS_KEY" \
     -var "aws_secret_key=$AWS_SECRET_KEY" \
     -var "image_version=$CMEM_IMAGE_VERSION" \
     -var "aws_region=$AWS_REGION" \
-    cmem-docker.json
+    /cmem-docker.json
