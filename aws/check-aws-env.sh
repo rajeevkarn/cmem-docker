a4df26d713dbec96f917d39415c140620e0a1d2b
Ivan Ermilov <earthquakesan@gmail.com>
2018-06-03 15:36:00 +0200

    remove generate-packer-template, use packer vars instead

--- aws/check-aws-env.sh
+++ aws/check-aws-env.sh
@@ -5,3 +5,5 @@ IFS=$'\n\t'
 : ${AWS_ACCESS_KEY?"You are required to set AWS_ACCESS_KEY (AWS access key for your AWS account) environment variable."}
 : ${AWS_SECRET_KEY?"You are required to set AWS_SECRET_KEY (AWS secret key for your AWS account) environment variable."}
 : ${AWS_INSTANCE_TYPE?"You are required to set AWS_INSTANCE_TYPE (AWS instance type for CMEM) environment variable. The recommended minimum setup is t2.large"}
+: ${AWS_REGION?"You are required to set AWS_REGION (AWS region) environment variable. The recommended parameter for Europe is eu-central-1"}
+: ${CMEM_IMAGE_VERSION?"You are required to set CMEM_IMAGE_VERSION (AMI version) environment variable."}
--- aws/packer/make-ami.sh
+++ aws/packer/make-ami.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-export AWS_REGION="eu-central-1"
-export CMEM_IMAGE_VERSION="1.0.0"
-
 wget https://releases.hashicorp.com/packer/1.2.3/packer_1.2.3_linux_amd64.zip
 unzip -o packer_1.2.3_linux_amd64.zip
-export AWS_ACCESS_KEY=$(grep "AWS_ACCESS_KEY" ../aws.conf | cut -d' ' -f 2)
-export AWS_SECRET_KEY=$(grep "AWS_SECRET_KEY" ../aws.conf | cut -d' ' -f 2)
-if [ -z "$AWS_ACCESS_KEY" ]; then
-  echo "You are required to set AWS_ACCESS_KEY (AWS access key for your AWS account) in aws.conf file."
-  exit 1
-fi
-if [ -z "$AWS_SECRET_KEY" ]; then
-  echo "You are required to set AWS_SECRET_KEY (AWS secret key for your AWS account) in aws.conf file."
-  exit 1
-fi
 
-./generate-packer-template.sh > cmem-docker.json
 ./packer validate cmem-docker.json
 
 ./packer build \
     -var "aws_access_key=$AWS_ACCESS_KEY" \
     -var "aws_secret_key=$AWS_SECRET_KEY" \
     -var "image_version=$CMEM_IMAGE_VERSION" \
+    -var "aws_region=$AWS_REGION" \
     cmem-docker.json
